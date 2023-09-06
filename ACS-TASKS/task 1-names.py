7b6cda2f61c579e601bb917c1bafc42f81ed9c23
ctepickering <pickeringcte@dulwich.org.uk>
2023-09-06 10:20:08 +0100

    starter task 1

---
+++ ACS-TASKS/task 1-names.py
@@ -0,0 +1,5 @@
+#user enters first and surname and full name is returned
+firstname = input ("Please enter your first name ")
+surname = input ("Please enter your surname ")
+fullname = firstname + " " + surname
+print ("Your full name is "+ fullname)
