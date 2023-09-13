8950bbd5dd2cbf072b813aad2042b12b8566f779
ctepickering <pickeringcte@dulwich.org.uk>
2023-09-13 12:05:35 +0100

    slight tweaks

--- ACS-TASKS/task 12-factors.py
+++ ACS-TASKS/task 12-factors.py
@@ -1,4 +1,5 @@
 #user inputs number
+count = 0
 user_num=int(input("Please enter a number to see their factors : "))
 #checks if every number up to user_num is a factor
 for i in range(user_num):
@@ -6,7 +7,11 @@ for i in range(user_num):
         #eliminates 1 and the same number as they arent factors
         if i +1 == 1 or i+1 == user_num:
             next
+            if count == 0 and user_num == i+1:
+                print("This number has no factors")
+            #end if
         else :
             print(i+1)
+            count=count+1
         #end if
     #end if
