1c134b5270a3bd94be416efcbbb0d6cefab69ed8
ctepickering <pickeringcte@dulwich.org.uk>
2023-10-02 11:22:41 +0100

    Update task 15-bowling.py

--- ACS-TASKS/task 15-bowling.py
+++ ACS-TASKS/task 15-bowling.py
@@ -1,33 +1,16 @@
-POINTS_FOR_STRIKE = 10
-
-class BowlingPlayer(object):
-    def __init__(self, name, overall=0):
-        self.name = name
-        self.total_points = overall
-
-    def frames(self):
-        for frame in range(1, 11):
             print(f'Frame {frame}:')
             shot_one = int(input('how many pins on first shot:'))
             if shot_one == POINTS_FOR_STRIKE:
                 # this is a strike
-                shot_two_after_strike = int(input('how many pins on shot after strike:'))
-                self.total_points += shot_two_after_strike + shot_one
-                if shot_two_after_strike == POINTS_FOR_STRIKE:
-                     # you got another strike. continue the frame!
-                    shot_three_after_strike = int(input('how many pins on shot after two strikes:'))
-                    self.total_points += shot_three_after_strike
-            elif shot_one < POINTS_FOR_STRIKE:
-                shot_two = int(input('how many pins on second shot:'))
-                shots = shot_one + shot_two
-                self.total_points += shots
-                if shots == POINTS_FOR_STRIKE:
-                    # this is a spare
-                    shot_three_after_spare = int(input('how many pins on shot after spare'))
-                    self.total_points += shot_three_after_spare
+#bowling game
+scores=[]
+for turns in range(10):
+    frame()
 
-        return self.total_points
-
-if __name__ == '__main__':
-    craig = BowlingPlayer('craig')
-    print(craig.frames())
+def frame() :
+    totalscore=0
+    turn1 =0
+    print("Frame " + str(turns) + ": ")
+    #prints which frame you are on e.g 'Frame 1:'
+    turn1 = input("Please enter the number of pins you have knocked down")
+    
