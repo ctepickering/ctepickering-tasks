f862df8f58d55bebb30ae0bdfd39cf8f1be08872
ctepickering <pickeringcte@dulwich.org.uk>
2023-10-06 14:44:08 +0100

    good

--- PYGAME/pong.py
+++ PYGAME/pong.py
@@ -1,6 +1,8 @@
 import pygame
-import math
 import random
+
+# Initialize the game engine
+pygame.init()
 
 # Define some colors
 white = ((255,255,255))
@@ -30,43 +32,57 @@ tan = ((230,220,170))
 coffee_brown =((200,190,140))
 moon_glow = ((235,245,255))
 colours = [blue,green,red,yellow,purple,pink,orange,blue_green]
-size = (800, 500)
-screen = pygame.display.set_mode(size)
-done=False
 
+#screen dimensions
+size = (800, 500)
+#defines screen
+screen = pygame.display.set_mode(size)
+#sets done to false so the program loops
+done=False
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Pong by Charlie")
 # Starting position of the rectangle
 rect_x = 380
 rect_y = 230
-count=0
+
 user_y = 190
 follow_y=190
 # Speed and direction of rectangle
 x_change = 5
 y_change = 5
 follow_change = 3
-clock = pygame.time.Clock()
+#starting background colour
 random1=blue
-#font = pygame.font.SysFont("Calibri", 25, True, False)
-#text = font.render("GAME OVER",True,black)
- 
-# Put the image of the text on the screen at 250x250
-#screen.blit(text, [250, 250])
+#sets number of lives
+lives=5
+end = ""
 # -------- Main Program Loop -----------
 while done == False:
 
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
- 
+    
     # Set the screen background
     screen.fill(random1)
     # Draw the rectangle
     pygame.draw.rect(screen, white, [rect_x, rect_y, 20, 20])
     pygame.draw.rect(screen, black, [rect_x, rect_y, 20, 20], 2)
+
+    # Select the font to use, size, bold, italics
+    font = pygame.font.SysFont('Calibri', 50, True, False)
+    text = font.render("Lives : " +str(lives), True, black) 
+    # Put the image of the text on the screen at 250x250
+    screen.blit(text, [315,10])
+
+    endmessage = font.render(end, True, black)
+    screen.blit(endmessage, [270,100])
+    #draw the barriers
     player=pygame.draw.rect(screen, black, [20, user_y, 20, 120])
     pygame.draw.rect(screen, black, [760, user_y, 20, 120])
     
-    # Move the rectangle starting point
+    # Move barriers
     key=pygame.key.get_pressed()
     if key[pygame.K_UP]:
         user_y=user_y-5
@@ -76,17 +92,28 @@ while done == False:
     
     rect_x = rect_x + x_change
     rect_y = rect_y + y_change
+    #to bounce off barriers
     if rect_x==40 or rect_x ==740 :
         if ((user_y > rect_y and user_y < (rect_y + 120))or ((user_y + 120) > rect_y and (user_y + 120) < (rect_y + 120))):
             x_change = x_change * -1
             random1= random.choice(colours)
+    #to bounce off side wall
     if rect_y > 480 or rect_y < 0:
         y_change = y_change * -1
         random1= random.choice(colours)
-    #elif rect_x > 780 or rect_x <0:
-        #Put the image of the text on the screen at 250x250
-        #screen.blit(text, [250, 250])
+    elif rect_x > 780 or rect_x <0:
+       rect_x = 380
+       rect_y = 230 
+       lives = lives-1
     pygame.display.flip()
+    #ends after 5 lives have been used
+    if lives == 0:
+        rect_x = 380
+        rect_y = 230
+        x_change=0
+        y_change=0
+        end = "GAME OVER"
 
+    #end if
     clock.tick(60)
 #end while
--- SRC-TASKS/task 1-numberinput.py
+++ SRC-TASKS/task 1-numberinput.py
@@ -1,5 +1,6 @@
 #array declaration
 num=[0 for _ in range(6)]
+total=0
 for i in range(6):
     num[i]=int(input("Please enter a number : "))
     total = total+num[i]
@@ -10,5 +11,5 @@ for i in range(5,-1,-1):
     print(num[i])
 #next i
 #average
-print("Total :", )
-print("Average :" , )
+print("Total :", total)
+print("Average :" , total/6)
