0b396f5756f61eb92816997ee7af9c7c4100c16b
ctepickering <pickeringcte@dulwich.org.uk>
2023-11-22 12:13:51 +0000

    finished invaders

--- PYGAME/spaceinvaders.py
+++ PYGAME/spaceinvaders.py
@@ -1,29 +1,36 @@
 import pygame
-import random
 import math
-# Define some colors
+import random
+import time
 
+
+
+
+
+
+# Initialize the game engine
 pygame.init()
 
-white = ((255,255,255))
-blue = ((0,0,255))
-green = ((0,255,0))
-red = ((255,0,0))
-black = ((0,0,0))
-orange = ((255,100,10))
-yellow = ((255,255,0))
-blue_green = ((0,255,170))
-marroon = ((115,0,0))
-lime = ((180,255,100))
-pink = ((255,100,180))
-purple = ((240,0,255))
-gray = ((127,127,127))
-magenta = ((255,0,230))
+# Define some colors
+WHITE = ((255,255,255))
+BLUE = ((0,0,255))
+GREEN = ((0,255,0))
+RED = ((255,0,0))
+BLACK = ((0,0,0))
+ORANGE = ((255,100,10))
+YELLOW = ((255,255,0))
+BLUE_GREEN = ((0,255,170))
+MARROON = ((115,0,0))
+LIME = ((180,255,100))
+PINK = ((255,100,180))
+PURPLE = ((240,0,255))
+GREY = ((127,127,127))
+MAGENTA = ((255,0,230))
 brown = ((100,40,0))
 forest_green = ((0,50,0))
 navy_blue = ((0,0,100))
 rust = ((210,150,75))
-dandilion_yellow = ((255,200,0))
+DANDILION_YELLOW = ((255,200,0))
 highlighter = ((255,255,100))
 sky_blue = ((0,255,255))
 light_gray = ((200,200,200))
@@ -32,123 +39,159 @@ tan = ((230,220,170))
 coffee_brown =((200,190,140))
 moon_glow = ((235,245,255))
 
-pygame.display.set_caption("Space invaders")
-
-
-# set screen
-size = (800, 500)
+#lives = 5
+end = ""
+text_font = pygame.font.SysFont(None, 30)
+font = pygame.font.SysFont(None, 30)
+screen_x =600
+screen_y = 500
+size = (screen_x, screen_y)
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Space Invaders")
 
-# Snowflake class
-class invader(pygame.sprite.Sprite):
-    #constructor function
-    def __init__(self,in_width,in_height):
-        self.size_x = in_width
-        self.size_y =in_height
-        self.image = pygame.Surface([self.size_x, self.size_y])
-        self.rect=self.image.get_rect()
-        self.image.fill(red)
-        self.rect.x = random.randint(0, 800)
-        self.rect.y = random.randint(-50,0)
-        self.speed = 1
-        self.speed2=0
-    #end of con func
-    def update(self):
-        self.rect.y += self.speed
-        self.rect.x +=self.speed2
-        if self.rect.y > 500:
-            self.rect.y = random.randint(-50,0)
-            self.rect.x = random.randint(0, 800)
-#end of class snow
-
-class Player(pygame.sprite.Sprite) :
-    def __init__(self, p_width, p_length, initial_x):
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+#classes go here
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, b_width , b_length):
         super().__init__()
-        self.width = p_width
-        self.height = p_length
+        self.image = pygame.Surface((5, 10))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = b_width
+        self.rect.centery = b_length
+    def update(self) :
+        self.rect.y = self.rect.y - 5
+
+#endclass
+class spaceship(pygame.sprite.Sprite):
+    def __init__(self, s_width, s_length, initial_x):
+        super().__init__()
+        self.x_val2 = x_val2
+        self.width = s_width
+        self.height = s_length
         self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(dandilion_yellow), 
+        self.image.fill(WHITE), 
         self.rect = self.image.get_rect()
         self.rect.x = initial_x
-        self.rect.y = 470
-        self.speed=5
+        self.rect.y = 465
+
+    
+
     def update(self):
-        key=pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-            self.rect.x= self.rect.x-self.speed
-        elif key[pygame.K_RIGHT]:
-            self.rect.x= self.rect.x+self.speed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 5
+
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.rect.right > 800:
-            self.rect.right = 800
+        if self.rect.right > 700:
+            self.rect.right = 700
 
-class Bullet(pygame.sprite.Sprite) :
-    def __init__(self):
         
-        self.image = pygame.Surface([2, 5]) 
-        self.rect=self.image.get_rect()
-        self.bullet_count = 50
-        self.rect.x = 400
-        self.rect.y = 400
-        self.speed = 0
-        self.color = black
-    def update(self):
-        self.rect.y = self.rect.y-3
-        pygame.draw.rect(screen, self.color , [self.rect.x,self.rect.y,2,5])
 
+class Invader(pygame.sprite.Sprite):
+    def __init__(self, I_width, I_height ):
+        super().__init__()
+        self.width = I_width
+        self.height = I_height
+        self.speed = random.randrange(1,3)
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(PURPLE)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0,600)
+        self.rect.y = 0
+        self.horizontalspeed = -1
+
+    def update(self):
+        if self.rect.y >= -50 :
+            self.rect.y = self.rect.y - self.horizontalspeed
+        if self.rect.y > screen_y:
+            self.rect.y = -50
+            
+       
+
+
+#global variables
+x_val2 = 350
+enemy_count = 5
+x_val = 0
+y_val = 200
+x_offset = 1
+pi= 3.141592652
+counter = 0
+end = ""
+score = 0
 
 #create sprite groups
 all_sprites = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 invader_sprites = pygame.sprite.Group()
-player_sprite = pygame.sprite.Group()
+spaceship_sprite = pygame.sprite.Group()
+
+# create player spaceship
+player = spaceship(40 , 30 , x_val2)
+spaceship_sprite.add(player)
+
+#set the enemy count
+Invader_Num = enemy_count
 
 
-#-Global Varaibles
-done=False
-colours = [blue,green,red,purple,pink,orange,blue_green]
-
-number_of_invaders=10
-for i in range(number_of_invaders) :
-    enemy = invader(30,30)
-    invader_sprites.add(enemy)
-player= Player(20,20,390)
-
-clock = pygame.time.Clock()
-
-
+ 
 # -------- Main Program Loop -----------
-while done == False:
-
+while not done:
+    # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
-        
-        bullet=Bullet()
-        bullets.add(bullet)
-        all_sprites.add(bullet)
-         
- 
-    # Set the screen background
-    screen.fill(black)
-
-    #draw 
-    font = pygame.font.SysFont('Calibri', 20, True, False)
-    text = font.render("Lives : "+str(player.lives), True, white) 
-   
-    screen.blit(text, [700,10])
-
-    player.update()
-    bullet.update()
-    invader_sprites.update()
-
-    #game logic
-    player_hit_group=pygame.sprite.spritecollide(player,invader_group,True)
-    for foo in player_hit_group:
-        player.lives=player.lives-1
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            bullet = Bullet(player.rect.centerx, player.rect.top)
+            bullets.add(bullet)
+            all_sprites.add(bullet)
     
-    pygame.display.flip()
+    if len(invader_sprites) < Invader_Num:
+        Invader_width = 30
+        Invader_Length = 30 
+        enemy = Invader(Invader_width, Invader_Length)
+        invader_sprites.add(enemy)
+        all_sprites.add(enemy)
+        score = score + 1
+    # --- Game logic should go here
 
+    #check for collisions
+    hits = pygame.sprite.groupcollide(invader_sprites, bullets, True, True)
+    
+    #update game objects
+    all_sprites.update()
+    spaceship_sprite.update()
+
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    
+    
+    screen.fill(BLACK)
+    
+    #draw stuff here:
+    all_sprites.draw(screen)
+    spaceship_sprite.draw(screen)
+    
+    #messages at the top 
+    realscore = score - 5
+    score_count = font.render("Enemies Hit: " + str(realscore), True, WHITE)
+
+    screen.blit(score_count, [10,10])
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
     clock.tick(60)
-#end while
+#endwhile
+pygame.quit()
