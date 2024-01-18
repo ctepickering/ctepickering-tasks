f14d1449a4f66d48df2d8ffe4ea7135c904765e7
ctepickering <pickeringcte@dulwich.org.uk>
2024-01-18 09:06:32 +0000

    Update fibbonacci.py

--- SRC-TASKS/Recursion/fibbonacci.py
+++ SRC-TASKS/Recursion/fibbonacci.py
@@ -1,3 +1,20 @@
+#iterative fibbonacci
+def fibbonacciI(n): 
+    for i in range(n):
+        if i <2:
+            num=1
+            num2=num
+            num3=num2
+        else :
+            num=num2+num3
+            num3=num2
+            num2=num
+
+    return num
+#endfunc
+print(fibbonacciI(8))
+
+
 #recursive fibbonacci
 def fibbonacciR(n): 
     if n== 0:
@@ -6,15 +23,18 @@ def fibbonacciR(n):
         return 1
     return fibbonacciR(n-1) + fibbonacciR(n-2)
 #endfunc
-print(fibbonacciR(4))
-#iterative fibbonacci
-def fibbonacciI(n): 
-    for i in range(n):
-        if i <3:
-            num = 1
-        else :
-            num2=num
-            num = num + num2
-    return num
+print(fibbonacciR(8))
+
+
+#recursive dynamic fibbonacci
+fibDict={}
+def fibbonacciRD(n): 
+    if n<=1:
+        fibDict[n]=1
+        return 1
+    else:
+       fibDict[n]= fibbonacciRD(n-1) + fibbonacciRD(n-2)
+       return fibbonacciR(n-1) + fibbonacciR(n-2)
 #endfunc
-print(fibbonacciI(4))
+fibbonacciRD(8)
+print(fibDict)
