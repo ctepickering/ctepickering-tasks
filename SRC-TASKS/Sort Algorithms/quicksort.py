a4958d5319265612b4d0d043307cc7fd6928b4d0
ctepickering <pickeringcte@dulwich.org.uk>
2024-01-25 11:32:07 +0000

    Update quicksort.py

--- SRC-TASKS/Sort Algorithms/quicksort.py
+++ SRC-TASKS/Sort Algorithms/quicksort.py
@@ -1,17 +1,36 @@
-sumnums=[7,4,6,8,1,5]
-
-def quicksort(arr):
-    pivot=len(arr)-1
-    pointer=1
-    while pointer != pivot :
-        if arr[pivot]> arr[pointer]:
-            temp = arr[pointer]
-            arr[pointer]=arr[pivot]
-            arr[pivot]=temp
-            pivot = pivot-1 
-            pointer = pointer+1
-        else :
-            pivot = pivot-1 
-            pointer = pointer+1
-
-print(quicksort(sumnums))
+def arrSwap(arr,a,b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp 
+#end procedure
+    
+def quickSplit(arr,ptr,pivot):
+    direction = 1
+    while ptr != pivot:
+        if ((direction ==  1 and arr[ptr] > arr[pivot]) or
+            (direction == -1 and arr[ptr] < arr[pivot])):
+            arrSwap(arr, ptr, pivot)   #swap values                     
+            temp = ptr #swap pointers
+            ptr = pivot
+            pivot = temp
+            direction = direction * -1 #change direction
+        #end if
+        ptr += direction
+    #end while
+    return pivot
+#end function
+        
+def quickSort(arr,left,right):
+    if left >= right: 
+        pass #base case reached
+    else:
+        pivotPos = quickSplit(arr,left,right)  
+        quickSort(arr,left,pivotPos-1)
+        quickSort(arr,pivotPos+1,right)
+    #end if
+#end procedure
+                    
+data = [1,12,10,3,5,2,9,7]
+print(data)
+quickSort(data,0,len(data)-1)
+print(data)
