b578a25902f08c628a76501ee8b7605cc78968d9
ctepickering <pickeringcte@dulwich.org.uk>
2024-03-11 11:30:15 +0000

    Update treasureislandgame.py

--- SRC-TASKS/treasureislandgame.py
+++ SRC-TASKS/treasureislandgame.py
@@ -1,25 +1,28 @@
 
 class Treasure:
     def __init__(self, value:int, level:str) -> None:
-        self.value = value
-        self.level = level
+        self.__value = value
+        self.__level = level
     #end constructor
 
     def getValue(self):
-        return self.value
+        return self.__value
     #end function
     
     def getLevel(self):
-        return self.level
+        return self.__level
     #end function
     def setValue(self, newVal:int) :
-        self.value = newVal
+        self.__value = newVal
     #end procedure
         
     def setLevel(self, newLevel:str) :
-        self.value = newLevel
+        self.__level = newLevel
     #end procedure
 #end class
         
 gold_cup = Treasure(500,"Gold")
-print(gold_cup.value)
+
+gold_cup.__value = 300
+print(gold_cup._Treasure__value,gold_cup._Treasure__level)
+print(gold_cup.getValue(),gold_cup.getLevel())
