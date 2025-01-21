b3f3b0a49a9bd6be3fc5ef1115d8ee27483b3dc8
ctepickering <pickeringcte@dulwich.org.uk>
2025-01-21 18:33:09 +0000

    Update linearSearch.py

--- SRC-TASKS/Search Algorithms/linearSearch.py
+++ SRC-TASKS/Search Algorithms/linearSearch.py
@@ -9,3 +9,18 @@ def linearSearch(arr,item):
         #end if 
     #end while
     if not found :
+        return -1
+    #end if
+#end function
+index = 0
+item = 5
+arr = [1,2,6,7,8,3,4,55,6,3,5,1,5,8,9,10,4]
+def linearSearchRec(arr,item,i) :
+    if i == len(arr) :
+        return -1
+    elif arr[i] == item :
+        return i
+    else :
+        return linearSearchRec(arr,item, i+1)
+    
+print(linearSearchRec(arr, item, index))
